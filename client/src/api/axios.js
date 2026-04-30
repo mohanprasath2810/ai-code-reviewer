94361761d315024eb7a9992beb91188985d21256
Mohana Prasath <mohanaprasath828@gmail.com>
2026-04-30 20:51:45 +0530

    updated API URL for production

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,4 +1,7 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000/api" });
+const API = axios.create({ 
+  baseURL: "https://ai-code-reviewer-t1jj.onrender.com/api" 
+});
+
 export default API;
